e7113938cb0e468094bc4bd065e46de12e7870de
Michael Fiano <mail@michaelfiano.com>
2018-03-13 12:57:16 -0400

    Moved transient state to funcs.el

--- packages.el
+++ packages.el
@@ -119,35 +119,6 @@
             ("mr" . "repl")
             ("mS" . "stickers")
             ("mt" . "trace")))
-    (spacemacs|define-transient-state common-lisp-navigation
-      :title "Common Lisp Navigation Transient State"
-      :doc "
-
-^^Definitions                           ^^Compiler Notes             ^^Stickers
-^^^^^^─────────────────────────────────────────────────────────────────────────────────────
-[_g_] Jump to definition                [_n_] Next compiler note     [_s_] Next sticker
-[_G_] Jump to definition (other window) [_N_] Previous compiler note [_S_] Previous sticker
-[_b_] Pop from definition
-
-[_q_] Exit
-"
-  :foreign-keys run
-  :bindings
-  ("g" sly-edit-definition)
-  ("G" sly-edit-definition-other-window)
-  ("b" sly-pop-find-definition-stack)
-  ("n" sly-next-note)
-  ("N" sly-previous-note)
-  ("s" sly-stickers-next-sticker)
-  ("S" sly-stickers-prev-sticker)
-  ("q" nil :exit t)))
-
-  (use-package sly-mrepl
-    :after sly
-    :bind
-    (:map sly-mrepl-mode-map
-          ("<up>" . sly-mrepl-previous-input-or-button)
-          ("<down>" . sly-mrepl-next-input-or-button))))
 
 (defun common-lisp-sly/init-sly-company ()
   (use-package sly-company
