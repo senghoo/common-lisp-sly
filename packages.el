6a238d65e9cad5e5e32f86fb64d51347b250f1be
Michael Fiano <mail@michaelfiano.com>
2018-06-13 12:10:40 -0400

    updated to work on latest develop branch

--- packages.el
+++ packages.el
@@ -1,26 +1,21 @@
 (defconst common-lisp-sly-packages
   '((common-lisp-snippets :requires yasnippet)
     xterm-color
-    persp-mode
     popwin
     helm
+    evil
     evil-cleverparens
     parinfer
-    sly
-    (sly-company :requires (sly company))
+    company
+    smartparens
+    (sly :requires smartparens)
+    (sly-mrepl :requires sly :location built-in)
     (sly-macrostep :requires (sly macrostep))
     (sly-repl-ansi-color :requires sly)))
 
 (defun common-lisp-sly/pre-init-xterm-color ()
   (when (configuration-layer/package-usedp 'sly)
     (add-hook 'sly-mrepl-mode-hook (lambda () (setq xterm-color-preserve-properties t)))))
-
-(defun common-lisp-sly/pre-init-persp-mode ()
-  (spacemacs|use-package-add-hook persp-mode
-    :post-config
-    (def-persp-buffer-save/load :mode 'sly-mrepl-mode :tag-symbol 'def-sly-buffer
-      :save-vars '(major-mode)
-      :after-load-function (lambda (b &rest _) (with-current-buffer b (sly))))))
 
 (defun common-lisp-sly/pre-init-popwin ()
   (spacemacs|use-package-add-hook sly
@@ -32,7 +27,7 @@
 
 (defun common-lisp-sly/init-common-lisp-snippets ())
 
-(defun common-lisp-sly/post-init-helm ()
+(defun common-lisp-sly/pre-init-helm ()
   (spacemacs|use-package-add-hook sly
     :post-init
     (spacemacs/set-leader-keys-for-major-mode 'lisp-mode
@@ -47,18 +42,26 @@
 (defun common-lisp-sly/post-init-parinfer ()
   (add-hook 'lisp-mode-hook 'parinfer-mode))
 
+(defun common-lisp-sly/pre-init-evil ()
+  (with-eval-after-load 'evil
+    (when (configuration-layer/package-used-p 'sly)
+      (evil-set-initial-state 'sly-mrepl-mode 'insert)
+      (evil-set-initial-state 'sly-inspector-mode 'emacs)
+      (evil-set-initial-state 'sly-db-mode 'emacs)
+      (evil-set-initial-state 'sly-xref-mode 'emacs)
+      (evil-set-initial-state 'sly-stickers--replay-mode 'emacs))))
+
+(defun common-lisp-sly/pre-init-smartparens ()
+  (with-eval-after-load 'smartparens
+    (when (configuration-layer/package-used-p 'sly)
+      (sp-local-pair '(sly-mrepl-mode) "'" "'" :actions nil)
+      (sp-local-pair '(sly-mrepl-mode) "`" "`" :actions nil))))
+
 (defun common-lisp-sly/init-sly ()
   (use-package sly
     :defer t
     :init
     (spacemacs/register-repl 'sly 'sly)
-    (sp-local-pair '(sly-mrepl-mode) "'" "'" :actions nil)
-    (sp-local-pair '(sly-mrepl-mode) "`" "`" :actions nil)
-    (evil-set-initial-state 'sly-mrepl-mode 'insert)
-    (evil-set-initial-state 'sly-inspector-mode 'emacs)
-    (evil-set-initial-state 'sly-db-mode 'emacs)
-    (evil-set-initial-state 'sly-xref-mode 'emacs)
-    (evil-set-initial-state 'sly-stickers--replay-mode 'emacs)
     (setq sly-autodoc-use-multiline t
           sly-complete-symbol*-fancy t
           sly-kill-without-query-p t
@@ -112,33 +115,28 @@
     (mapc (lambda (x)
             (spacemacs/declare-prefix-for-mode 'lisp-mode (car x) (cdr x)))
           '(("mc" . "compile")
-            ("me" . "evaluate")
+            ("m" . "evaluate")
             ("mg" . "navigation")
             ("mh" . "help")
             ("mm" . "macro")
             ("mr" . "repl")
             ("mS" . "stickers")
-            ("mt" . "trace")))
-    (use-package sly-mrepl
-      :after sly
-      :bind
-      (:map sly-mrepl-mode-map
-            ("<up>" . sly-mrepl-previous-input-or-button)
-            ("<down>" . sly-mrepl-next-input-or-button)
-            ("<C-up>" . sly-mrepl-previous-input-or-button)
-            ("<C-down>" . sly-mrepl-next-input-or-button)))))
+            ("mt" . "trace")))))
 
-(defun common-lisp-sly/init-sly-company ()
-  (use-package sly-company
-    :config
-    (setf sly-company-completion 'fuzzy)
-    (add-hook 'sly-mode-hook 'sly-company-mode)
-    (add-hook 'sly-mrepl-hook 'sly-company-mode)
-    (add-to-list 'company-backends 'sly-company)))
+(defun common-lisp-sly/init-sly-mrepl ()
+  (use-package sly-mrepl
+    :after sly
+    :bind
+    (:map sly-mrepl-mode-map
+          ("<up>" . sly-mrepl-previous-input-or-button)
+          ("<down>" . sly-mrepl-next-input-or-button)
+          ("<C-up>" . sly-mrepl-previous-input-or-button)
+          ("<C-down>" . sly-mrepl-next-input-or-button))))
 
 (defun common-lisp-sly/post-init-company ()
-  '(push '(company-sly company-capf company-dabbrev-code) company-backends-lisp-mode)
-  (spacemacs|add-company-backends lisp-mode))
+  (spacemacs|add-company-backends
+    :backends company-capf company-files
+    :modes sly-mode sly-mrepl-mode))
 
 (defun common-lisp-sly/init-sly-macrostep ()
   (use-package sly-macrostep
